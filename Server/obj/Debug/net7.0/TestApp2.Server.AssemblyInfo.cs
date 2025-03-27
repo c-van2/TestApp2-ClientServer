5c678d909b21f071e86d268c4b2e8de7bdda4ed5
Vanessa <vanessachan@amgwealth.com>
2025-03-27 09:41:15 +0800

    Setup:  Add MudBlazor and Test with Button Component

--- Server/obj/Debug/net7.0/TestApp2.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/TestApp2.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8400c9f56de3e5761320c214138ac5fe23b21c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
